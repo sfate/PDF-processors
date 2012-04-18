94350528d60548b73b313c705349930e220e18d9
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2012-04-18 15:23:17 +0300

    fix paths

--- pdf_creator.rb
+++ pdf_creator.rb
@@ -9,47 +9,43 @@ puts "Welcome"
 puts "Starting PDF-creator..."
 
 def create_new_file(recieved_message, request)
-    locale_fop_path = "/home/sfate/prj/last_odt/vendor/fop-1.0"
-    #locale_fop_path = "/home/last_odt/vendor/fop-1.0"
-    p "> > > recieved_message #{recieved_message.inspect}"
-    p "> > > request #{request}"
-    p "recieved_message['src'] = #{recieved_message['src']}"
-    name_of_fo = File.basename(recieved_message["src"], '.fo')
-    p ">>> name_of_fo #{name_of_fo}"
-    fo_receive= name_of_fo
-    sleep 2
-    status_to_xml = {
-      :condition => "ok",
-      :error_message => ""
-    }
-      path = "#{File.dirname(recieved_message["src"]).gsub(/tmp\/fo/,"")}public/files/completed_pdf/"
-      complete_pdf_path = "#{path}#{name_of_fo}.pdf"
-      Dir::mkdir(path) if !File.directory? path
+  current_dir = File.join(Dir.pwd, '..', '/vendor/fop-1.0')
+  name_of_fo = File.basename(recieved_message["src"], '.fo')
+  fo_receive= name_of_fo
+  sleep 2
+  status_to_xml = {
+    :condition => "ok",
+    :error_message => ""
+  }
+  path = "#{File.dirname(recieved_message["src"])}"
+  complete_pdf_path = "#{path}#{name_of_fo}.pdf"
+  Dir::mkdir(path) if !File.directory? path
 
-      # We generate the classpath by scanning the FOP lib directory
-      command = "java -cp #{locale_fop_path}/build/fop.jar"
-      Dir.foreach("#{locale_fop_path}/lib") do |file|
-        command << ":#{locale_fop_path }/lib/#{file}" if (file.match(/.jar/))
-      end
-      command << " org.apache.fop.cli.Main "
-      command << " -fo #{recieved_message['src']}"
+  # We generate the classpath by scanning the FOP lib directory
+  command = "java -cp #{locale_fop_path}/build/fop.jar"
+  Dir.foreach("#{locale_fop_path}/lib") do |file|
+      command << ":#{locale_fop_path }/lib/#{file}" if (file.match(/.jar/))
+  end
+  command << " org.apache.fop.cli.Main "
+  command << " -fo #{recieved_message['src']}"
  #    command << " -xsl #{locale_fop_path}/xslt/doc2fo.xsl"
-      command << " -pdf #{complete_pdf_path}"
-      puts "command is #{command}"
-      if(system command) then
-        puts "Yahoo, it's done!'"
-      else
-        status_to_xml[:condition] =     "error"
-        status_to_xml[:error_message] = "Failed to make pdf."
-        puts "creating pdf fail :'("
-      end
+  command << " -pdf #{complete_pdf_path}"
+  if(system command) then
+    puts "Done. Exiting..."
+  else
+    status_to_xml.update({
+      :condition     => "error",
+      :error_message => `#{command}`
+    }
+    puts "Fail. Look at error message in queue for details"
+  end
 
-    data_to_send = {
-  	  :pdf_url => "#{complete_pdf_path}",
-      :error => status_to_xml
-    }.to_json
+  data_to_send = {
+    :pdf_url => "#{complete_pdf_path}",
+    :error => status_to_xml
+  }.to_json
 
-    request.publish(data_to_send)
+  request.publish(data_to_send)
 end
 
 recieved_message = ""
